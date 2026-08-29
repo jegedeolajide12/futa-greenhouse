a521edcc24e69315ea4323cc78b77a4b68a9d772
Jegede Olajide <jegedeolajide1@gmail.com>
2026-08-29 11:46:29 +0100

    added admin again

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
 # Create superuser (if it doesn't exist)
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.get_or_create(username='admin', defaults={'email':'admin@example.com', 'is_superuser':True, 'is_staff':True})" | python manage.py shell
+# Create superuser from environment variable
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); user, created = User.objects.get_or_create(username='admin', defaults={'email':'admin@example.com', 'is_superuser':True, 'is_staff':True}); if created: user.set_password('$ADMIN_PASSWORD'); user.save()" | python manage.py shell
 
 echo "Build complete."
